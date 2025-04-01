3b3754d05dcd689e2c8292780985368d6a00c51c
cwild-UoS <c.wild@sheffield.ac.uk>
2025-04-01 20:01:36 +0100

    Init commit of gpx file reader

---
+++ fitness_plotter/fitness_plotter/strava/gpx_file.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import gpxpy
+
+class GPXFile:
+    def __init__(self, file_path):
+        self.file_path = Path(file_path)
+        with open(self.file_path) as f:
+            self.gpx = gpxpy.parse(f)
+
+    @property
+    def polyline(self):
+        polyline = []
+        for record in self.gpx.tracks[0].segments[0].points:
+            polyline.append([record.latitude, record.longitude])
+        return polyline
+
+    @property
+    def sport(self):
+        return self.gpx.tracks[0].name
